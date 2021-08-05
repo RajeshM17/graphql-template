bc9782f70ead5998fe7ebdf43d3ef92388107fe7
Rajesh Malviya <rajeshmalviya1710@gmail.com>
2021-08-05 18:09:54 +0530

    GraphQL-server-tempelate

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express=require("express");
+const app=express();
+const PORT=6969;
+const {graphqlHTTP}=require("express-graphql");
+
+const schema=require("./Schemas/index")
+
+//Quering :- receiving the data
+
+//Mutation :- to update the data
+
+
+//creating graphql server
+app.use('/graphql',graphqlHTTP({
+    schema,
+    graphiql:true
+}))
+
+app.listen(PORT,()=>{
+    console.log("server started");
+})
